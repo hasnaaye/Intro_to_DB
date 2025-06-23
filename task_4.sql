7503c695cda9fb2158f71e0173bdd398af7ba05a
hasnaaye <hasnaeyerrou@gmail.com>
2025-06-23 23:36:48 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,4 @@
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_NAME = 'Books'
-    AND TABLE_SCHEMA = DATABASE();
+select column_name, data_type
+from information_schema.columns
+where table_name = 'books'
+and table_schema = 'alx_book_store';
